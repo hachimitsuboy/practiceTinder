e0ae829f89237e840acb93c97836211cf8700179
hachimitsuboy <39763423+hachimitsuboy@users.noreply.github.com>
2021-07-25 19:56:20 +0900

    チャプター６完了

--- MatchingApp/View/CardView.swift
+++ MatchingApp/View/CardView.swift
@@ -5,6 +5,9 @@
 //  Created by Nagae on 2021/07/25.
 //
 
+//alpha値を調整することで、like,nopeを消えた様に見せる
+
+
 import UIKit
 
 class CardView: UIView {
@@ -14,11 +17,15 @@ class CardView: UIView {
         let iv = UIImageView()
         iv.backgroundColor = .systemTeal
         iv.layer.cornerRadius = 12
-        iv.contentMode = .scaleToFill
+        iv.contentMode = .scaleAspectFill
+        iv.image = UIImage(named: "testImage")
+        //clipsToBoundsとは？
+        iv.clipsToBounds = true
         
         return iv
     }()
     
+    //名前用ラベル
     let nameLabel:UILabel = {
         
         let nameLabel = UILabel()
@@ -30,6 +37,7 @@ class CardView: UIView {
         return nameLabel
     }()
     
+    //住所用ラベル
     let residenceLabel:UILabel = {
         
         let residenceLabel = UILabel()
@@ -41,6 +49,7 @@ class CardView: UIView {
         return residenceLabel
     }()
     
+    //趣味用ラベル
     let hobbyLabel: UILabel = {
         
         let hobbyLabel = UILabel()
@@ -52,6 +61,7 @@ class CardView: UIView {
         return hobbyLabel
     }()
     
+    //自己紹介用のラベル
     let introductionLabel: UILabel = {
         
         let introductionLabel = UILabel()
@@ -64,7 +74,7 @@ class CardView: UIView {
     }()
     
     
-    
+    //情報を表示させるボタン
     let infoButton:UIButton = {
         
         let infoButton = UIButton(type: .system)
@@ -74,6 +84,44 @@ class CardView: UIView {
         
         return infoButton
         
+    }()
+    
+    let goodLabel:UILabel = {
+        
+        let goodLabel = UILabel()
+        //あらかじめフォントをBoldにしてくれる？
+        goodLabel.font = .boldSystemFont(ofSize: 46)
+        goodLabel.text = "GOOD"
+        goodLabel.textColor = .rgb(red: 137, green: 233, blue: 86)
+        //borderWidthとは？
+        goodLabel.layer.borderWidth = 3
+        //UIColorとCGColorの違いとは？
+        goodLabel.layer.borderColor = UIColor.rgb(red: 137, green: 233, blue: 86).cgColor
+        goodLabel.layer.cornerRadius = 12
+        //文字の配置
+        goodLabel.textAlignment = .center
+        //alpha値はどれだけ透かすか 0だと透明
+        goodLabel.alpha = 0
+        
+        
+        return goodLabel
+    }()
+    
+    let nopeLabel:UILabel = {
+        
+        let nopeLabel = UILabel()
+        nopeLabel.font = .boldSystemFont(ofSize: 46)
+        nopeLabel.text = "NOPE"
+        nopeLabel.textColor = .rgb(red: 222, green: 110, blue: 110)
+        
+        nopeLabel.layer.borderWidth = 3
+        nopeLabel.layer.borderColor = UIColor.rgb(red: 222, green: 110, blue: 110).cgColor
+        nopeLabel.layer.cornerRadius = 12
+        nopeLabel.textAlignment = .center
+        nopeLabel.alpha = 0
+        
+        
+        return nopeLabel
     }()
     
     override init(frame: CGRect) {
@@ -94,19 +142,55 @@ class CardView: UIView {
         let translation = gesture.translation(in: self)
         //わからん
         if gesture.state == .changed{
-            //わからん
-            self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
-            
+           
+            handlePanChage(translation: translation)
         } else if gesture.state == .ended { //ジェスチャーが終わったら？
-            //0.2秒かけてアニメーションを行う
-            UIView.animate(withDuration: 0.32) {
-                //場所を元に戻す？
-                self.transform = .identity
-                //わからん
-                self.layoutIfNeeded()
-            }
+           
+            handlePanEnded()
+        }
+    }
+    
+    private func handlePanChage(translation: CGPoint){
+        //回転をつけてるみたい（わからん）↓↓↓↓↓↓↓
+        let degree:CGFloat = translation.x / 20
+        let angle = degree * .pi / 100
+        let rotateTranslation = CGAffineTransform(rotationAngle: angle)
+        self.transform = rotateTranslation.translatedBy(x: translation.x, y: translation.y)
+        
+        //alpha値をこのratioによって1/100値で設定する
+        //カードのxの値によって管理するためにかけている
+        //translationはcardImageが右に行くとプラスの値を取り、左に行くとマイナスの値を取る
+        let ratio: CGFloat = 1 / 100
+        let ratioValue = ratio * translation.x
+        
+        if translation.x > 0{
+            
+            self.goodLabel.alpha = ratioValue
+        } else if translation.x < 0 {
+            
+            self.nopeLabel.alpha = -ratioValue
         }
         
+        
+        //↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+    }
+    
+    //元の位置に戻った時
+    private func handlePanEnded(){
+        
+        //この引数たちは？わからん
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.7, options: []) {
+            
+            //場所を元に戻す？
+            self.transform = .identity
+            //わからん
+            self.layoutIfNeeded()
+            
+            self.goodLabel.alpha = 0
+            self.nopeLabel.alpha = 0
+           
+            
+        }
     }
     
     private func setupLayout(){
@@ -122,6 +206,8 @@ class CardView: UIView {
         addSubview(cardImageView)
         addSubview(nameLabel)
         addSubview(baseStackView)
+        addSubview(goodLabel)
+        addSubview(nopeLabel)
         
         //引数には、対応するView（親のView？っていうのかな？)の値を入れる
         cardImageView.anchor(top:topAnchor, bottom: bottomAnchor, left: leftAnchor, right: rightAnchor, leftPadding: 10, rightPadding: 10)
@@ -131,6 +217,10 @@ class CardView: UIView {
         baseStackView.anchor(bottom: cardImageView.bottomAnchor, left: cardImageView.leftAnchor, right: cardImageView.rightAnchor, bottomPadding: 20, leftPadding: 20, rightPadding: 20)
         
         nameLabel.anchor(bottom: baseStackView.topAnchor, left: cardImageView.leftAnchor, bottomPadding: 20, leftPadding: 22)
+        
+        goodLabel.anchor(top: cardImageView.topAnchor, left: cardImageView.leftAnchor, width: 140, height: 60, topPadding: 25, leftPadding: 22)
+        
+        nopeLabel.anchor(top: cardImageView.topAnchor, right: cardImageView.rightAnchor, width: 140, height: 60, topPadding: 25, rightPadding: 22)
         
     }
     
